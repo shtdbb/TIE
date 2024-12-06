35ceae1fd7b92f22cf1849eb775a9adbd1105126
shtdbb <shtdbb@qq.com>
2024-12-06 20:14:15 +0800

    add special token

---
+++ script/add_special_token.py
@@ -0,0 +1,11 @@
+from transformers import BertTokenizer
+
+model_path = "model/ernie3_base"
+tokenizer = BertTokenizer.from_pretrained(model_path)
+
+tokenizer.bos_token = tokenizer.cls_token
+tokenizer.eos_token = tokenizer.sep_token
+special_tokens = ["[Y]", "[M]", "[D]", "<memory>", "</memory>"]
+tokenizer.add_special_tokens({'additional_special_tokens': special_tokens})
+
+tokenizer.save_pretrained("model/tie")
