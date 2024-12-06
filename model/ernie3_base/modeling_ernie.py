24c8736fd90f2bea5bc64c9dbab0405fba74168f
shtdbb <shtdbb@qq.com>
2024-12-06 20:16:03 +0800

    add annotation `modeling_ernie.py`

--- model/ernie3_base/modeling_ernie.py
+++ model/ernie3_base/modeling_ernie.py
@@ -76,23 +76,23 @@ class ErnieEmbeddings(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
-        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
-        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)   # 40000, 768
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)    # 2048, 768
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)   # 4, 768
         self.use_task_id = config.use_task_id
         if config.use_task_id:
-            self.task_type_embeddings = nn.Embedding(config.task_type_vocab_size, config.hidden_size)
+            self.task_type_embeddings = nn.Embedding(config.task_type_vocab_size, config.hidden_size)   # 3. 768
 
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
-        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)   # 768, 1e-5
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)   # 0.1
         # position_ids (1, len position emb) is contiguous in memory and exported when serialized
         self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
-        self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
+        self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))   # (1, 2048)
         self.register_buffer(
             "token_type_ids", torch.zeros(self.position_ids.size(), dtype=torch.long), persistent=False
-        )
+        )   # (1, 2048)
 
     def forward(
         self,
@@ -125,23 +125,23 @@ class ErnieEmbeddings(nn.Module):
                 token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
 
         if inputs_embeds is None:
-            inputs_embeds = self.word_embeddings(input_ids)
-        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+            inputs_embeds = self.word_embeddings(input_ids)   # (bs, s, h)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)   # (bs, s, h)
 
-        embeddings = inputs_embeds + token_type_embeddings
+        embeddings = inputs_embeds + token_type_embeddings   # (bs, s, h)
         if self.position_embedding_type == "absolute":
-            position_embeddings = self.position_embeddings(position_ids)
+            position_embeddings = self.position_embeddings(position_ids)   # (bs, s, h)
             embeddings += position_embeddings
 
         # add `task_type_id` for ERNIE model
         if self.use_task_id:
             if task_type_ids is None:
                 task_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
-            task_type_embeddings = self.task_type_embeddings(task_type_ids)
+            task_type_embeddings = self.task_type_embeddings(task_type_ids)   # (bs, s, h)
             embeddings += task_type_embeddings
 
-        embeddings = self.LayerNorm(embeddings)
-        embeddings = self.dropout(embeddings)
+        embeddings = self.LayerNorm(embeddings)   # (bs, s, h)
+        embeddings = self.dropout(embeddings)   # (bs, s, h)
         return embeddings
 
 
@@ -159,7 +159,7 @@ class ErnieSelfAttention(nn.Module):
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
         self.all_head_size = self.num_attention_heads * self.attention_head_size
 
-        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)   # (768, 768) + bias
         self.key = nn.Linear(config.hidden_size, self.all_head_size)
         self.value = nn.Linear(config.hidden_size, self.all_head_size)
 
@@ -174,9 +174,9 @@ class ErnieSelfAttention(nn.Module):
         self.is_decoder = config.is_decoder
 
     def transpose_for_scores(self, x: torch.Tensor) -> torch.Tensor:
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(new_x_shape)
-        return x.permute(0, 2, 1, 3)
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)   #   (bs, s, n, h')
+        x = x.view(new_x_shape)   #   (bs, s, h) -> (bs, s, n, h')
+        return x.permute(0, 2, 1, 3)   #   (bs, s, n, h') -> (bs, n, s, h')
 
     def forward(
         self,
@@ -188,7 +188,7 @@ class ErnieSelfAttention(nn.Module):
         past_key_value: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
         output_attentions: Optional[bool] = False,
     ) -> Tuple[torch.Tensor]:
-        mixed_query_layer = self.query(hidden_states)
+        mixed_query_layer = self.query(hidden_states)   # (bs, s, h) -> (bs, s, h)
 
         # If this is instantiated as a cross-attention module, the keys
         # and values come from an encoder; the attention mask needs to be
@@ -210,10 +210,10 @@ class ErnieSelfAttention(nn.Module):
             key_layer = torch.cat([past_key_value[0], key_layer], dim=2)
             value_layer = torch.cat([past_key_value[1], value_layer], dim=2)
         else:
-            key_layer = self.transpose_for_scores(self.key(hidden_states))
-            value_layer = self.transpose_for_scores(self.value(hidden_states))
+            key_layer = self.transpose_for_scores(self.key(hidden_states))   # (bs, n, s, h')
+            value_layer = self.transpose_for_scores(self.value(hidden_states))   # (bs, n, s, h')
 
-        query_layer = self.transpose_for_scores(mixed_query_layer)
+        query_layer = self.transpose_for_scores(mixed_query_layer)   # (bs, n, s, h')
 
         use_cache = past_key_value is not None
         if self.is_decoder:
@@ -227,7 +227,7 @@ class ErnieSelfAttention(nn.Module):
             past_key_value = (key_layer, value_layer)
 
         # Take the dot product between "query" and "key" to get the raw attention scores.
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))   # (bs, n, s, s)
 
         if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
             query_length, key_length = query_layer.shape[2], key_layer.shape[2]
@@ -251,7 +251,7 @@ class ErnieSelfAttention(nn.Module):
                 relative_position_scores_key = torch.einsum("bhrd,lrd->bhlr", key_layer, positional_embedding)
                 attention_scores = attention_scores + relative_position_scores_query + relative_position_scores_key
 
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)   # (bs, n, s, s)
         if attention_mask is not None:
             # Apply the attention mask is (precomputed for all layers in ErnieModel forward() function)
             attention_scores = attention_scores + attention_mask
@@ -261,23 +261,23 @@ class ErnieSelfAttention(nn.Module):
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
-        attention_probs = self.dropout(attention_probs)
+        attention_probs = self.dropout(attention_probs)   # (bs, n, s, s)
 
         # Mask heads if we want to
         if head_mask is not None:
             attention_probs = attention_probs * head_mask
 
-        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = torch.matmul(attention_probs, value_layer)   # (bs, n, s, h')
 
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()   # (bs, s, n, h')
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(new_context_layer_shape)
+        context_layer = context_layer.view(new_context_layer_shape)   # (bs, s, h)
 
         outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
 
         if self.is_decoder:
             outputs = outputs + (past_key_value,)
-        return outputs
+        return outputs   # (bs, s, h)
 
 
 # Copied from transformers.models.bert.modeling_bert.BertSelfOutput with Bert->Ernie
@@ -289,7 +289,7 @@ class ErnieSelfOutput(nn.Module):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
-        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dense(hidden_states)   # (bs, s, h)
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
@@ -332,17 +332,17 @@ class ErnieAttention(nn.Module):
         output_attentions: Optional[bool] = False,
     ) -> Tuple[torch.Tensor]:
         self_outputs = self.self(
-            hidden_states,
-            attention_mask,
+            hidden_states,   # b, s, h
+            attention_mask,   # b, n, s, s
             head_mask,
             encoder_hidden_states,
             encoder_attention_mask,
             past_key_value,
             output_attentions,
-        )
-        attention_output = self.output(self_outputs[0], hidden_states)
+        )   # [(bs, s, h), ]
+        attention_output = self.output(self_outputs[0], hidden_states)   # (bs, s, h)
         outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
-        return outputs
+        return outputs   # # [(bs, s, h), ]
 
 
 # Copied from transformers.models.bert.modeling_bert.BertIntermediate with Bert->Ernie
@@ -356,9 +356,9 @@ class ErnieIntermediate(nn.Module):
             self.intermediate_act_fn = config.hidden_act
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        hidden_states = self.dense(hidden_states)
-        hidden_states = self.intermediate_act_fn(hidden_states)
-        return hidden_states
+        hidden_states = self.dense(hidden_states)   # (bs, s, 4*h)
+        hidden_states = self.intermediate_act_fn(hidden_states)   # (bs, s, 4*h)
+        return hidden_states   # (bs, s, 4*h)
 
 
 # Copied from transformers.models.bert.modeling_bert.BertOutput with Bert->Ernie
@@ -370,10 +370,10 @@ class ErnieOutput(nn.Module):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
-        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dense(hidden_states)   # (bs, s, h)
         hidden_states = self.dropout(hidden_states)
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
-        return hidden_states
+        return hidden_states   # (bs, s, h)
 
 
 # Copied from transformers.models.bert.modeling_bert.BertLayer with Bert->Ernie
@@ -405,13 +405,13 @@ class ErnieLayer(nn.Module):
         # decoder uni-directional self-attention cached key/values tuple is at positions 1,2
         self_attn_past_key_value = past_key_value[:2] if past_key_value is not None else None
         self_attention_outputs = self.attention(
-            hidden_states,
-            attention_mask,
+            hidden_states,   # (bs, s, h)
+            attention_mask,   # (bs, n, s, s)
             head_mask,
             output_attentions=output_attentions,
             past_key_value=self_attn_past_key_value,
-        )
-        attention_output = self_attention_outputs[0]
+        )   # [(bs, s, h), ]
+        attention_output = self_attention_outputs[0]   # (bs, s, h)
 
         # if decoder, the last output is tuple of self-attn cache
         if self.is_decoder:
@@ -449,16 +449,16 @@ class ErnieLayer(nn.Module):
         layer_output = apply_chunking_to_forward(
             self.feed_forward_chunk, self.chunk_size_feed_forward, self.seq_len_dim, attention_output
         )
-        outputs = (layer_output,) + outputs
+        outputs = (layer_output,) + outputs   # [(bs, s, h), ]
 
         # if decoder, return the attn key/values as the last output
         if self.is_decoder:
             outputs = outputs + (present_key_value,)
 
-        return outputs
+        return outputs   # [(bs, s, h), ]
 
     def feed_forward_chunk(self, attention_output):
-        intermediate_output = self.intermediate(attention_output)
+        intermediate_output = self.intermediate(attention_output)   # (bs, s, 4*h)
         layer_output = self.output(intermediate_output, attention_output)
         return layer_output
 
@@ -528,9 +528,9 @@ class ErnieEncoder(nn.Module):
                     encoder_attention_mask,
                     past_key_value,
                     output_attentions,
-                )
+                )   # [(bs, s, h), ]
 
-            hidden_states = layer_outputs[0]
+            hidden_states = layer_outputs[0]   # (bs, s, h)
             if use_cache:
                 next_decoder_cache += (layer_outputs[-1],)
             if output_attentions:
@@ -612,9 +612,9 @@ class ErnieLMPredictionHead(nn.Module):
         self.decoder.bias = self.bias
 
     def forward(self, hidden_states):
-        hidden_states = self.transform(hidden_states)
+        hidden_states = self.transform(hidden_states)   # (bs, s, h)
         hidden_states = self.decoder(hidden_states)
-        return hidden_states
+        return hidden_states   # (bs, s, 40000)
 
 
 # Copied from transformers.models.bert.modeling_bert.BertOnlyMLMHead with Bert->Ernie
@@ -920,7 +920,8 @@ class ErnieModel(ErniePreTrainedModel):
         # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
         # ourselves in which case we just need to make it broadcastable to all heads.
         extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
-
+        # 扩展维度到四维, (bs, n, s, s)
+        
         # If a 2D or 3D attention mask is provided for the cross-attention
         # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
         if self.config.is_decoder and encoder_hidden_states is not None:
@@ -939,18 +940,18 @@ class ErnieModel(ErniePreTrainedModel):
         # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
         head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
 
-        embedding_output = self.embeddings(
-            input_ids=input_ids,
+        embedding_output = self.embeddings(   # (bs, s, h)
+            input_ids=input_ids,   # (b, s)
             position_ids=position_ids,
-            token_type_ids=token_type_ids,
+            token_type_ids=token_type_ids,   # (b, s)
             task_type_ids=task_type_ids,
             inputs_embeds=inputs_embeds,
             past_key_values_length=past_key_values_length,
         )
         encoder_outputs = self.encoder(
             embedding_output,
-            attention_mask=extended_attention_mask,
-            head_mask=head_mask,
+            attention_mask=extended_attention_mask,   # (bs, n, s, s)
+            head_mask=head_mask,   # (n_layers, )
             encoder_hidden_states=encoder_hidden_states,
             encoder_attention_mask=encoder_extended_attention_mask,
             past_key_values=past_key_values,
@@ -958,8 +959,8 @@ class ErnieModel(ErniePreTrainedModel):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
-        )
-        sequence_output = encoder_outputs[0]
+        )   # output对象，存储最后一个layer的输出 hidden_states: (bs, s, h)
+        sequence_output = encoder_outputs[0]   # (bs, s, h)
         pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
 
         if not return_dict:
@@ -1314,10 +1315,10 @@ class ErnieForMaskedLM(ErniePreTrainedModel):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
-        )
+        )   # output对象，存储最后一个layer的输出 hidden_states: (bs, s, h)
 
-        sequence_output = outputs[0]
-        prediction_scores = self.cls(sequence_output)
+        sequence_output = outputs[0]   # (bs, s, h)
+        prediction_scores = self.cls(sequence_output)   # (bs, s, vocab_size)
 
         masked_lm_loss = None
         if labels is not None:
